c4938d1ccf339cbe4c840dac4d43425db591207c
Emmanuel Akins <Akins1412>
2024-02-19 10:16:06 -0800

    Biggie

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -25,3 +25,29 @@ void inpution_sort_list(listint_t **list)
 		}
 	}
 }
+
+
+/**
+ * nodees_switchs - Swap two nodes in a listint_t doubly-linked list.
+ * in descending order. mediumer elements to the left.
+ *
+ * @top: A pointer to the top of the doubly-linked list.
+ * @large: a ptr to the node with larger `n` (data).
+ * @medium: a ptr to the node with mediumer `n` (data).
+ */
+void nodees_switchs(listint_t **top, listint_t **large, listint_t *medium)
+{
+	(*large)->next = medium->next;
+	if (medium->next != NULL)  /*Not the last node*/
+		medium->next->prev = *large; /*node3 point back to node1*/
+
+	medium->prev = (*large)->prev;  /*point to node b4 node1*/
+	medium->next = *large;
+
+	if ((*large)->prev != NULL) /*not the first node (top)*/
+		(*large)->prev->next = medium; /*node0 point to medium*/
+	else
+		*top = medium;
+	(*large)->prev = medium;
+	*large = medium->prev;
+}
